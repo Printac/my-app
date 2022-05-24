0660d2b15e64317217a0aee1eab986be01e02a75
Ben Printac <bprintac1@gmail.com>
2022-05-24 13:58:30 +0300

    Updates

---
+++ precache-manifest.1200fd5a1a2dbecfae0a198440f40a4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "87e6c5627e626c968bd0",
+    "url": "/react-learning/static/css/main.f7d7246a.chunk.css"
+  },
+  {
+    "revision": "87e6c5627e626c968bd0",
+    "url": "/react-learning/static/js/main.7b4e5394.chunk.js"
+  },
+  {
+    "revision": "920750df5cc4e33ea907",
+    "url": "/react-learning/static/js/runtime~main.e0a07a4e.js"
+  },
+  {
+    "revision": "68718060231b1d9c125b",
+    "url": "/react-learning/static/css/2.3afc3c4c.chunk.css"
+  },
+  {
+    "revision": "68718060231b1d9c125b",
+    "url": "/react-learning/static/js/2.a55fe2cd.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/react-learning/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "22004d11fb9ebaf5dc43f899705cc155",
+    "url": "/react-learning/index.html"
+  }
+];
